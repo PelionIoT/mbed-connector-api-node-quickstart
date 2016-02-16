40c4bac579ff140854fa57deb0347941227bd6a7
Brian <brianddaniels@gmail.com>
2016-02-16 19:52:57 +0000

    Adding websocket functionality

--- app.js
+++ app.js
@@ -1,34 +1,103 @@
 var express = require('express');
+var path = require('path');
 var MbedConnector = require('mbed-connector');
 var app = express();
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
 
 /* BEGIN EDITS */
 //var mbedConnectorAccessKey = "Your Access Key";
-//var endpointName = "Your endpoint name";
 var port = process.env.PORT || 3000;
 /* END EDITS */
 
-var mbedConnector = new MbedConnector("https://api.connector.mbed.com", { token: mbedConnectorAccessKey });
+var mbedConnector = new MbedConnector({ accessKey: mbedConnectorAccessKey });
+
+var sockets = [];
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hbs');
 
 app.get('/', function (req, res) {
-  mbedConnector.getResource(endpointName, 'Test/0/D', function(error, data) {
+  mbedConnector.getEndpoints(function(error, endpoints) {
     if (error) {
-      var msg = 'There was an error when trying to get resource "Test/0/D" from endpoint "' + endpointName + '".<br>'
-      msg += 'Is your device connected properly?<br><br>';
-
-      msg += 'HTTP status code: ' + error.status + '<br>';
-      msg += 'Response body: ' + error.response.body;
-
-      res.send(msg);
+      res.send(String(error));
     } else {
-      res.send('Button Presses: ' + data);
+      res.render('index', {
+        endpoints: endpoints
+      });
     }
   });
 });
 
-var server = app.listen(port, function () {
+app.use(function(err, req, res, next) {
+  console.log(err.stack);
+  res.status(err.status || 500);
+  res.render('error', {
+    message: err.message,
+    error: err
+  });
+});
+
+io.on('connection', function (socket) {
+  sockets.push(socket);
+
+  socket.on('subscribe-to-presses', function (data) {
+    console.log('subscribe-to-presses', data);
+    mbedConnector.putResourceSubscription(data.endpointName, 'Test/0/D', function(error) {
+      console.log('putResourceSubscription', error);
+      if (error) {
+        console.log(String(error));
+      } else {
+        socket.emit('subscribed-to-presses', {
+          endpointName: data.endpointName
+        });
+      }
+    });
+  });
+
+  socket.on('unsubscribe-to-presses', function (data) {
+    console.log('unsubscribe-to-presses', data);
+    mbedConnector.deleteResourceSubscription(data.endpointName, 'Test/0/D', function(error) {
+      console.log('deleteResourceSubscription', error);
+      if (error) {
+        console.log(String(error));
+      } else {
+        socket.emit('unsubscribed-to-presses', {
+          endpointName: data.endpointName
+        });
+      }
+    });
+  });
+
+  socket.on('get-presses', function (data) {
+    console.log('get-presses', data);
+    mbedConnector.getResourceValue(data.endpointName, 'Test/0/D', function(error, value) {
+      console.log('getResourceValue', error, value);
+
+      socket.emit('presses', {
+        endpointName: data.endpointName,
+        value: value
+      });
+    });
+  });
+});
+
+server.listen(port, function () {
   console.log('mbed Device Connector Quickstart listening at http://localhost:%s', port);
 
   mbedConnector.startLongPolling();
 });
 
+mbedConnector.on('notifications', function(notifications) {
+  console.log('notifications', notifications);
+  notifications.forEach(function(notification) {
+    if (notification.path === '/Test/0/D') {
+      sockets.forEach(function(socket) {
+        socket.emit('presses', {
+          endpointName: notification.ep,
+          value: notification.payload
+        });
+      });
+    }
+  })
+})
